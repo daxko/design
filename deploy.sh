a62b23ba8f32b7e8482630883e39652b547de40d
Jason <jason@scurker.com>
2015-01-07 09:54:22 -0600

    cleaning up travis deploy script

--- deploy.sh
+++ deploy.sh
@@ -3,12 +3,11 @@
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "travis-ci"
 
+git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/daxko/design.git build > /dev/null
+rm -rf build/*
+
 grunt build && cd build
 
-git init --quiet
-git remote add origin https://${GH_TOKEN}@github.com/daxko/design.git
-git fetch origin
-git checkout --track origin/gh-pages --force
 git add --all
 git commit -m "Deploying ${TRAVIS_COMMIT} to gh-pages"
-git push origin gh-pages
+git push origin gh-pages --quiet > /dev/null
