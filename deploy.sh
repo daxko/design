125a688040515d357252eb104b624089c6d39402
Jason <jason@scurker.com>
2015-01-07 09:17:33 -0600

    setting deploy script to push against gh-pages

--- deploy.sh
+++ deploy.sh
@@ -2,12 +2,13 @@
 
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "travis-ci"
-git remote set-url origin "https://${GH_TOKEN}@github.com/daxko/design.git"
 
-grunt build
-cd build
+grunt build && cd build
 
-git clone --branch=gh-pages https://${GH_TOKEN}@github.com/daxko/design .
+git init --quiet
+git remote add origin https://${GH_TOKEN}@github.com/daxko/design.git
+git fetch origin
+git checkout --track origin/gh-pages --force
 git add --all
 git commit -m "Deploying ${TRAVIS_COMMIT} to gh-pages"
-git push
+git push origin gh-pages
