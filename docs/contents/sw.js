b05acaa04d179cb3f7c29c0ef1d2d2fa7287a391
Jason <jason@scurker.com>
2016-05-24 13:23:59 -0500

    Adding sw.js for offline fallbacks

---
+++ docs/contents/sw.js
@@ -0,0 +1,49 @@
+'use strict';
+
+var cacheUrls = [
+  '/',
+  '/css/styleguide.css',
+  '/images/daxko.png',
+  '/js/scrollspy.js',
+  '/js/dropdown.js',
+  '/js/fixed-sticky.js'
+];
+
+var cacheVersion = 'v1';
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(cacheVersion).then(function(cache) {
+      return cache.addAll(cacheUrls);
+    })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  caches.keys().then(function(keys) {
+    console.log(keys);
+    return Promise.all(
+      keys
+        .filter(function(key) { return key !== cacheVersion; })
+        .map(function(key) { return caches.delete(key); })
+    );
+  })
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(networkFirst(event.request));
+});
+
+// Fetch request from the network first, fallback to cache if network request fails
+function networkFirst(request) {
+  return fetch(request).then(function(response) {
+    if(response && response.status === 200) {
+      caches.open(cacheVersion).then(function(cache) {
+        cache.put(request, response.clone());
+      });
+    }
+    return response;
+  }).catch(function() {
+    return caches.match(request);
+  });
+}
