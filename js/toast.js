bef2696fefee1a31c266858c86b8ccbd2e5dc2d1
Matt Miller <matt.miller@daxko.com>
2023-01-25 15:22:45 -0500

    OPS-35364: Update let to var in toast.js

--- js/toast.js
+++ js/toast.js
@@ -1,20 +1,20 @@
 // A function to create a toast message using Toastify JS
 function createToast(query) {
   // Element for the toast
-  let element = document.querySelector(query);
+  var element = document.querySelector(query);
   // The inner element of the dax-toast elemnent that contains all of the toast message
-  let inner = element.getElementsByClassName('dax-toast-inner')[0];
+  var inner = element.getElementsByClassName('dax-toast-inner')[0];
   // Class names to be added to the outer toastify element
-  let classList = [];
+  var classList = [];
   element.classList.forEach(c => {
     if(c !== 'dax-toast')
       classList.push(c);
   });
 
   // Configurable properties of the toast message
-  let gravity = element.dataset.gravity ?? "top"; // `top` or `bottom`
-  let position = element.dataset.position ?? "right"; // `left`, `center` or `right`
-  let duration = element.dataset.duration ?? 5000;
+  var gravity = element.dataset.gravity ?? "top"; // `top` or `bottom`
+  var position = element.dataset.position ?? "right"; // `left`, `center` or `right`
+  var duration = element.dataset.duration ?? 5000;
 
   // Create and show the toast message
   Toastify({
